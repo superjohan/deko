402aa309eee35b2f45b0b1b1e3d0886f4f7ccba4
Johan Halin <johan@halin.me>
2014-11-20 18:37:04 +0200

    Removed commented IAP manager functionality.

--- deko/DekoIAPManager.m
+++ deko/DekoIAPManager.m
@@ -8,123 +8,42 @@
 
 #import "DekoIAPManager.h"
 
-NSString * const DekoProVersionIdentifier = @"com.aerodeko.deko.pro";
-NSString * const DekoProVersionPurchasedKey = @"DekoProVersionPurchasedKey";
 NSString * const DekoIAPManagerProPriceUpdatedNotification = @"DekoIAPManagerProPriceUpdatedNotification";
 NSString * const DekoIAPManagerProVersionPurchasedNotification = @"DekoIAPManagerProVersionPurchasedNotification";
 
-/*
-@interface DekoIAPManager () <SKProductsRequestDelegate>
-@property (nonatomic) SKProduct *product;
-@end
-*/
-
 @implementation DekoIAPManager
-
-#pragma mark - Private
-
-- (void)_proPurchased
-{
-	AELOG_DEBUG(@"");
-	
-	[[NSUserDefaults standardUserDefaults] setBool:YES forKey:DekoProVersionPurchasedKey];
-	[[NSUserDefaults standardUserDefaults] synchronize];
-	
-	[[NSNotificationCenter defaultCenter] postNotificationName:DekoIAPManagerProVersionPurchasedNotification object:self];
-}
 
 #pragma mark - Public
 
 - (void)startManager
 {
-	/*
-	[PFPurchase addObserverForProduct:DekoProVersionIdentifier block:^(SKPaymentTransaction *transaction)
-	{
-		AELOG_DEBUG(@"%@", transaction);
-		
-		[self _proPurchased];
-	}];
-	
-	SKProductsRequest *productRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:[NSSet setWithObject:DekoProVersionIdentifier]];
-	productRequest.delegate = self;
-	[productRequest start];
-	 */
+	// Implementation removed
 }
 
 - (void)purchaseProVersion:(void (^)(NSError *error))completionBlock
 {
-	/*
-	[PFPurchase buyProduct:DekoProVersionIdentifier block:^(NSError *error)
-	{
-		BOOL success = (error == nil);
-		
-		[[LocalyticsSession sharedLocalyticsSession] tagEvent:@"IAP purchase completed" attributes:@{@"success" : [NSNumber numberWithBool:success]}];
-		
-		completionBlock(error);
-	}];
-	 */
+	// Implementation removed
 }
 
 - (NSString *)priceForProVersion
 {
+	// Implementation removed
+
 	return @"FREE LOL";
-	
-	/*
-	if (self.product == nil)
-	{
-		AELOG_DEBUG(@"Product not received yet.");
-		
-		return nil;
-	}
-	
-	NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
-	[numberFormatter setFormatterBehavior:NSNumberFormatterBehavior10_4];
-	[numberFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
-	[numberFormatter setLocale:self.product.priceLocale];
-	NSString *formattedString = [numberFormatter stringFromNumber:self.product.price];
-	
-	return formattedString;
-	 */
 }
 
 - (void)restorePurchases
 {
-	/*
-	[PFPurchase restore];
-	 */
+	// Implementation removed
 }
 
 #pragma mark - Properties
 
 - (BOOL)proPurchased
 {
+	// Implementation removed
+
 	return YES;
-
-	/* protip for all you jailbreak hackers out there
-	 
-	BOOL purchased = [[NSUserDefaults standardUserDefaults] boolForKey:DekoProVersionPurchasedKey];
-	
-	return purchased;
-	 */
 }
-
-#pragma mark - SKProductsRequestDelegate
-
-/*
-- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response
-{
-	if ([response.invalidProductIdentifiers count])
-	{
-		AELOG_ERROR(@"Invalid product identifiers: %@", response.invalidProductIdentifiers);
-	}
-	
-	SKProduct *product = [response.products lastObject];
-	self.product = product;
-	
-	AELOG_DEBUG(@"Local pro version price: %@", [self priceForProVersion]);
-	
-	[[NSNotificationCenter defaultCenter] postNotificationName:DekoIAPManagerProPriceUpdatedNotification object:self];
-}
-*/
 
 @end
